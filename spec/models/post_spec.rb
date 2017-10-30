020c535f79512690b162a204aa30a214a3332cb6
Suwijak Chaipipat <ppnk2537@gmail.com>
2017-10-30 14:39:29 +0700

    Extract docker-compose.test.yml and update circleci config

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -4,6 +4,12 @@ require 'rails_helper'
 
 RSpec.describe Post, type: :model do
   describe 'Anything' do
+    let(:post) do
+      Post.create(title: 'Hello, Docker', body: 'Rails on Docker')
+    end
+
+    subject { post }
+
     it { is_expected.to_not eq 0 }
   end
 end
